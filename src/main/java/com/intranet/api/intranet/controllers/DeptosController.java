0ef01b8f537251c0251b68f325bb12e76d180bc3
Mirko Gutierrez <mirko.gutierrez.it@gmail.com>
2024-06-20 16:55:36 -0400

    Se agregran los crossorigins

--- src/main/java/com/intranet/api/intranet/controllers/DeptosController.java
+++ src/main/java/com/intranet/api/intranet/controllers/DeptosController.java
@@ -3,6 +3,7 @@ package com.intranet.api.intranet.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.intranet.api.intranet.models.entities.Funcionario;
 import com.intranet.api.intranet.services.IFuncionariosService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api")
 public class DeptosController {
 
@@ -31,7 +33,8 @@ public class DeptosController {
     public List<Funcionario> showList(){
         return service.listFuncionarios();
     }
-
+    
+    
     @GetMapping("/buscar/{rut}")
     public Funcionario showFuncionario(@PathVariable Integer rut){
         return service.findByRut(rut);
