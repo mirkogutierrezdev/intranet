e984bc51c4605e0897169edcd99421121e9c64f0
Mirko Gutierrez <mirko.gutierrez.it@gmail.com>
2024-06-22 12:33:27 -0400

    agrega

--- src/main/java/com/intranet/api/intranet/repositories/IFuncionariossRepository.java
+++ src/main/java/com/intranet/api/intranet/repositories/IFuncionariossRepository.java
@@ -1,12 +1,11 @@
 package com.intranet.api.intranet.repositories;
 
-import java.util.List;
-import com.intranet.api.intranet.models.entities.Ausencias;
+
 import com.intranet.api.intranet.models.entities.Funcionario;
 
 public interface IFuncionariossRepository {
  
      Funcionario findByRut(Integer rut);
 
-     List<Ausencias> listAusencias(Integer rut);
+     
 }
